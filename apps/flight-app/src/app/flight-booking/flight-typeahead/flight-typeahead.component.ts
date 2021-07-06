277b1c4e6ca049263302273c1d936f1b037b5c00
michael.zikes <michael.zikes@intauria.com>
2021-07-06 14:32:10 +0200

    RxJS Typeahead

--- apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -1,6 +1,9 @@
+import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Flight } from '@flight-workspace/flight-lib';
 import { Observable, Subject, Subscription, timer } from 'rxjs';
-import { share, takeUntil, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, share, switchMap, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'flight-workspace-flight-typeahead',
@@ -12,10 +15,46 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   subscription = new Subscription();
   destroy$ = new Subject<void>();
 
-  constructor() { }
+  control = new FormControl();
+  loading: boolean;
+  flights$: Observable<Flight[]>;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.rxjsDemo();
+    // this.rxjsDemo();
+
+    // Stream 3: Result to render in Template
+    this.flights$ =
+      // Stream 1: Input field changes
+      // Trigger
+      // Data Provider
+      this.control.valueChanges.pipe(
+        // Filter START
+        filter(city => city.length > 2),
+        debounceTime(300),
+        distinctUntilChanged(),
+        // Filter END
+        // Side-Effect: set loading flag
+        tap(() => this.loading = true),
+        // Switch to Stream 2
+        switchMap(city => this.load(city)),
+        // Side-Effect: set loading flag
+        tap(() => this.loading = false)
+      );
+  }
+
+  // Stream 2: HTTP request to server resp. API
+  load(from: string): Observable<Flight[]>  {
+    const url = "http://www.angular.at/api/flight";
+
+    const params = new HttpParams()
+                        .set('from', from);
+
+    const headers = new HttpHeaders()
+                        .set('Accept', 'application/json');
+
+    return this.http.get<Flight[]>(url, {params, headers});
   }
 
   rxjsDemo(): void {
@@ -31,7 +70,7 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.destroy$.next();
+    // this.destroy$.next();
     // this.subscription.unsubscribe();
   }
 }
